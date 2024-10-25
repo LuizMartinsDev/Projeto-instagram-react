095093dfc4d8006698c63923b774a17f0ed2723a
LuizMartinsDev <luizkwdev@gmail.com>
2024-10-25 19:17:46 -0300

    realização das ações interativas da apliacação: like, save, edição de foto e edição de nome

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -8,16 +8,46 @@ import adorableAnimals from '../assets/usersSuggestions/adorable_animals.svg';
 import paperPlaneIcon from '../assets/icones/paper-plane-outline 1.png';
 import heartIcon from '../assets/icones/heart-outline 1.png';
 import vector from '../assets/icones/Vector.png';
+import vectorSave from '../assets/icones/salvar-instagram.png';
+import heartRed from '../assets/icones/coracao.png'
 import chat from '../assets/icones/chat.png';
 import ellipsis from '../assets/icones/ellipsis-horizontal 1.png'
 import styles from '../css/Posts.module.css'
+import { useState } from 'react';
 
 const Posts = () => {
+
+    const [activeIndex, setActiveIndex] = useState([]);
+    const [activeIndexHeart, setActiveIndexHeart] = useState([])
     const posts = [
-        {imageUser: meowed, nomeUser: "meowed", imagePost: gatoTelefone, imageLike: respondeai, userLike: "respondeai",  textoLike: "outras 101523 pessoas"},
-        {imageUser: barked, nomeUser: "barked", imagePost: cachorro, imageLike: adorableAnimals, userLike: "adorable_animals",  textoLike: "outras 99159 pessoas"},
-        {imageUser: meowed, nomeUser: "meowed", imagePost: yuyu, imageLike: respondeai, userLike: "respondeai",  textoLike: "outras 10259 pessoas"}
+        {imageUser: meowed, nomeUser: "meowed", imagePost: gatoTelefone, imageLike: respondeai, userLike: "respondeai",  textoLike: "outras 101523 pessoas", save: vector},
+        {imageUser: barked, nomeUser: "barked", imagePost: cachorro, imageLike: adorableAnimals, userLike: "adorable_animals",  textoLike: "outras 99159 pessoas", save: vector},
+        {imageUser: meowed, nomeUser: "meowed", imagePost: yuyu, imageLike: respondeai, userLike: "respondeai",  textoLike: "outras 10259 pessoas", save: vector}
     ]
+
+
+    const handleHeart = (index) => {
+        setActiveIndexHeart((prevIndex) => {
+            if(prevIndex.includes(index)) {
+                return prevIndex.filter(i => i !== index)
+            } else {
+                return [...prevIndex, index]
+            }
+        })
+    }
+
+    
+
+    const handleSave =(index) => {
+
+        setActiveIndex((prevIndex) => {
+            if(prevIndex.includes(index)){
+                return prevIndex.filter(i => i !== index)
+            } else {
+                return[...prevIndex, index]
+            }
+        })
+    }
     return(
         <section className={styles.container}>
             {posts.map((post, index) => (
@@ -33,11 +63,11 @@ const Posts = () => {
                     <img className={styles.imgPost} src={post.imagePost} alt="foto da publicação" />
                     <div className={styles.containerIcons}>
                         <div className={styles.icons}>
-                            <img src={heartIcon} alt="imagem de um icone de coração para like" />
+                            <img className={styles.iconHeart} onClick={() => handleHeart(index)} src={activeIndexHeart.includes(index) ? heartRed : heartIcon} alt="imagem de um icone de coração para like" />
                             <img src={chat} alt="imagem de um icone de comentário" />
                             <img src={paperPlaneIcon} alt="imagem de um icone de seta para compartilhamento" />
                         </div>
-                        <img className={styles.iconVector} src={vector} alt="imagem de um icone de vector para salvar o post" />
+                        <img onClick={() => handleSave(index)} className={styles.iconVector} src={activeIndex.includes(index) ? vectorSave : vector} alt="imagem de um icone de vector para salvar o post" />
                     </div>
                     <div className={styles.containerComment}>
                         <img src={post.imageLike} />
